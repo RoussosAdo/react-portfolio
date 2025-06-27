53713818d8e2de3a6dda029ccf1bf74a6e12ae32
OHESTSIS <roussos.ado@gmail.com>
2025-06-27 22:43:32 +0300

    Added more Icons

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,13 +1,20 @@
 import React, { Component } from "react";
 import { Link, useLocation } from "react-router-dom";
 import astronautHelmet from "../assets/astronaut-helmet.png"
+import deadEye from "../assets/dead-eye.png"
+import stack from "../assets/stack.png"
+import envelope from "../assets/envelope.png"
 import "../styles/nav.css";
+
 
 export default class Nav extends Component {
   render() {
     return (
       <div>
         <img src={astronautHelmet}/>
+        <img src={deadEye}/>
+        <img src={stack}/>
+        <img src={envelope}/>
       </div>
     );
   }
