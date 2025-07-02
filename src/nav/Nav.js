84edcfa8278a20fbc834e66cef9b9ecb4e11ce8e
OHESTSIS <roussos.ado@gmail.com>
2025-07-02 14:01:39 +0300

    Finished App.js and made some script updates

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -1,63 +1,59 @@
 import React, { Component } from "react";
 import { Link, useLocation } from "react-router-dom";
-import astronautHelmet from "../assets/astronaut-helmet.png"
-import deadEye from "../assets/dead-eye.png"
-import stack from "../assets/stack.png"
-import envelope from "../assets/envelope.png"
+import astronautHelmet from "../assets/astronaut-helmet.png";
+import deadEye from "../assets/dead-eye.png";
+import stack from "../assets/stack.png";
+import envelope from "../assets/envelope.png";
 import "../styles/nav.css";
 
+export default function Nav() {
+  const location = useLocation();
 
-export default  function Nav () {
-    const location = useLocation();
-
-    const getNavPositionClass = () => {
-      switch (location.pathname) {
-        case"/":
-          return "nav-about";
-        case"/skills":
-          return "nav-skills";
-        case"/projects":
-          return "nav-projects";
-        case"/contact":
-          return "nav-contact";
-        default:
-          return "";
-      }
-    };
-
-    const getPageTitle = () => {
-      switch (location.pathname) {
-        case"/":
-          return "ABOUT";
-        case"/skills":
-          return "SKILLS";
-        case"/projects":
-          return "PROJECTS";
-        case"/contact":
-          return "CONTACT";
-        default:
-          return "";
-      }
+  const getNavPositionClass = () => {
+    switch (location.pathname) {
+      case "/":
+        return "nav-about";
+      case "/skills":
+        return "nav-skills";
+      case "/projects":
+        return "nav-projects";
+      case "/contact":
+        return "nav-contact";
+      default:
+        return "";
     }
+  };
 
-    const navPositionClass = getNavPositionClass();
-    const pageTitle = getPageTitle();
+  const getPageTitle = () => {
+    switch (location.pathname) {
+      case "/":
+        return "ABOUT";
+      case "/skills":
+        return "SKILLS";
+      case "/projects":
+        return "PROJECTS";
+      case "/contact":
+        return "CONTACT";
+      default:
+        return "";
+    }
+  };
+
+  const navPositionClass = getNavPositionClass();
+  const pageTitle = getPageTitle();
 
   const isCurrentPage = (navClass) => {
-    return navClass === getNavPositionClass;
-  }
-
+    return navClass === navPositionClass;
+  };
 
   const renderNavLink = (to, imgSrc, altText, navClass) => {
     const isCurrent = isCurrentPage(navClass);
-    const linkClass = isCurrent ? "nav-link current" :
-    "nav-link"
+    const linkClass = isCurrent ? "nav-link current" : "nav-link";
 
     return (
       <Link to={to} className={linkClass}>
-          <img src={imgSrc} alt={altText}/>
-          {isCurrent && <h1 className="page-title">{pageTitle}
-          </h1>}
+        <img src={imgSrc} alt={altText} />
+        {isCurrent && <h1 className="page-title">{pageTitle}</h1>}
       </Link>
     );
   };
@@ -65,30 +61,14 @@ export default  function Nav () {
   return (
     <nav className={`nav ${navPositionClass}`}>
       {renderNavLink(
-        "/", 
+        "/",
         astronautHelmet,
         "astronaut helmet icon",
         "nav-about"
       )}
-      {renderNavLink(
-        "/skills", 
-        deadEye,
-        "deadEye icon",
-        "nav-skills"
-      )}
-      {renderNavLink(
-        "/projects", 
-        stack,
-        "stack icon",
-        "nav-projects"
-      )}
-      {renderNavLink(
-        "/contact", 
-        envelope,
-        "envelope icon",
-        "nav-contact"
-      )}
+      {renderNavLink("/skills", deadEye, "deadEye icon", "nav-skills")}
+      {renderNavLink("/projects", stack, "stack icon", "nav-projects")}
+      {renderNavLink("contact", envelope, "envelope icon", "nav-contact")}
     </nav>
-  )
+  );
 }
-
