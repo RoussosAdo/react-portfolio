39396dc7d6df8a6511a85e6d7043979bb74a7571
OHESTSIS <roussos.ado@gmail.com>
2025-07-14 04:03:16 +0300

    PlayerStats Creation

---
+++ src/playerStats/PlayerStats.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../style/playerStats.css";
+
+const playerStats = () => {
+  return (
+    <div id="playerStats">
+      <h1>ANTONIOS ROUSSOS LEVEL 21</h1>
+      <div id="playerStats__lines">
+        <span id="player__lines__thick"></span>
+        <span id="player__lines__thin"></span>
+      </div>
+    </div>
+  );
+};
