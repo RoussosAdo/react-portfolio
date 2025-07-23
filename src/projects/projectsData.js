b1047af7a5979f16611241bad7dbc5fce4281150
OHESTSIS <roussos.ado@gmail.com>
2025-07-23 15:58:38 +0300

    Project 1 Update

--- src/projects/projectsData.js
+++ src/projects/projectsData.js
@@ -1,21 +1,20 @@
-import projectOne from "../assets/project-1.png";
+import projectOne from "../assets/Blade Of Ashenfire.png";
 import projectTwo from "../assets/project-2.png";
 import projectThree from "../assets/project-3.png";
 
 const projects = {
   1: {
-    title: "SoMe Platform",
+    title: "Blades Of Ashenfire",
     image: projectOne,
     description: (
       <>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          Blades of Ashenfire is an intense 2D action-adventure game developed in Unity 2D. Set in a medieval fantasy world, it features fast-paced combat, elemental abilities, skill combos, and rich storytelling. Players journey with Aelara, a swordsmith warrior, battling dark forces and exploring beautifully crafted pixel-art environments filled with magic and danger.
         </p>
       </>
     ),
-    github: "https://github.com",
-    demo: "https://netlify.com",
+    github: "https://github.com/RoussosAdo/RPG-GAME-",
+    demo: "https://ohestisgames.itch.io/blades-of-ashenfire-demo",
   },
   2: {
     title: "Productivity App",
